a180a21d764073e20fde8c0b3bb0099d61629f30
Colin Rofls <colin@cmyr.net>
2016-11-10 19:29:18 -0500

    added basic test cases

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate unicode_segmentation;
+
+#[cfg(test)]
+mod tests {
+    use unicode_segmentation::UnicodeSegmentation;
+
+    #[test]
+    fn skin_tone_modifier() {
+        // http://unicode.org/reports/tr29/#WB14
+        let s = "😌👎🏼";
+        let w = s.split_word_bounds().collect::<Vec<&str>>();
+        let b: &[_] = &["😌", "👎🏼"];
+        assert_eq!(w, b);
+    }
+
+    #[test]
+    fn regional_indicator_symbols() {
+        // http://unicode.org/reports/tr29/#WB15
+        let s = "🇨🇦🇨🇭🇿🇲🇿 hi";
+        let w = s.split_word_bounds().collect::<Vec<&str>>();
+        let b: &[_] = &["🇨🇦", "🇨🇭", "🇿🇲", "🇿", "hi"];
+        assert_eq!(w, b);
+    }
+
+    #[test]
+    fn emoji_zwj_sequence() {
+        // http://unicode.org/reports/tr29/#WB3c
+        let s = "\u{1f468}\u{200d}\u{1f468}\u{200d}\u{1f466}";
+        let w = s.split_word_bounds().collect::<Vec<&str>>();
+        let b: &[_] = &["\u{1f468}\u{200d}\u{1f468}\u{200d}\u{1f466}"];
+        assert_eq!(w, b);
+    }
+}
